fe877398b87cd7f08cb5d29e173450fd802effc8
dadocadavero-debug <dadocadavero@gmail.com>
2026-09-08 17:41:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -317,9 +317,3 @@ app.get("/stream/tv/:id.json", async (req, res) => {
   res.json({ streams });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
